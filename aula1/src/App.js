3c6389d0e66df54dcc0f1b809db49687f3dc4bf9
ogatalars <ogataogara@gmail.com>
2022-11-10 14:53:20 -0300

    Using state on class

--- aula1/src/App.js
+++ aula1/src/App.js
@@ -1,16 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import { Component } from "react";
+import { Component } from "react";
 
-//componente de função sem estado
-function App() {
-  // é um componente porque está em letra maiscula
-  return (
-    <>
+// //componente de função sem estado
+// function App() {
+//   // é um componente porque está em letra maiscula
+//   return (
+//     <>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>{1 + 2} Olá mundo</p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//       <div> Elemento dentro do fragmento </div>
+//     </>
+//   );
+// }
+
+//componente de classe sem estado
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nome: "Ogata" };
+  }
+  render() {
+    const { nome } = this.state;
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{1 + 2} Olá mundo</p>
+          <p> {nome} </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,30 +50,9 @@ function App() {
           </a>
         </header>
       </div>
-      <div> Elemento dentro do fragmento </div>
-    </>
-  );
+    );
+  }
 }
-
-//componente de classe sem estado
-// class App extends Component {
-//   render() {
-//     return    <div className="App">
-//     <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <p>{1 + 2} Olá mundo</p>
-//     <a
-//       className="App-link"
-//       href="https://reactjs.org"
-//       target="_blank"
-//       rel="noopener noreferrer"
-//     >
-//       Learn React
-//     </a>
-//   </header>
-// </div>;
-//   }
-// }
 
 // {} -> o que tiver dentro é lido como código
 // podemos colocar dentro de um fragmento do react <> </> e ai conseguimos usar elementos que não necessariamente estão juntos
