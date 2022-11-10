b027641657e12616887716ca2126fc7f38f11780
ogatalars <ogataogara@gmail.com>
2022-11-10 15:10:24 -0300

    More about states and events

--- aula1/src/App.js
+++ aula1/src/App.js
@@ -31,22 +31,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { nome: "Ogata" };
+    this.state = { nome: "Ogata", counter: 0 };
   }
 
   handleClick() {
     this.setState({ nome: "Felipe" });
   }
+  handleAClick(event) {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
   render() {
-    const { nome } = this.state;
+    const { nome, counter } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{1 + 2} Olá mundo</p>
-          <p> {nome} </p>
+          <p>
+            {" "}
+            {nome}
+            {counter}
+          </p>
           <p onClick={this.handleClick}> </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
